571821cc406403504a83bb4758f6cddded6521e9
Anderson Miller <anderson@submarinerich.com>
2013-08-26 11:37:53 -0400

    adding password reset request

---
+++ daytoday/ResetPasswordRequest.h
@@ -0,0 +1,18 @@
+//
+//  ResetPasswordRequest.h
+//  daytoday
+//
+//  Created by Anderson Miller on 8/26/13.
+//  Copyright (c) 2013 Submarine Rich, LLC. All rights reserved.
+//
+
+#import "D2Request.h"
+
+@protocol ResetPasswordDelegate <D2RequestDelegate>
+- (void) passwordResetSuccessful;
+@end
+
+@interface ResetPasswordRequest : D2Request
+@property(nonatomic,weak) id <ResetPasswordDelegate> delegate;
+- (void) resetPassword:(NSString*)oldPassword newPassword:(NSString*)np;
+@end
